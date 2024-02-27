d62a27704d95864a9d97361ca3ca5bb8d2111809
Elizavetalii <sinitsyna-liza@inbox.ru>
2024-02-27 23:16:34 +0300

    Добавьте файлы проекта.

---
+++ AudioPlayer/ListeningHistory.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using static AudioPlayer.MainWindow;
+
+namespace AudioPlayer
+{
+    /// <summary>
+    /// Логика взаимодействия для ListeningHistory.xaml
+    /// </summary>
+    public partial class ListeningHistory : Window
+    {
+        public string SelectedAudio { get; set; }
+
+
+        public ListeningHistory()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBoxHistory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ListBoxHistory.SelectedItem != null)
+            {
+                SelectedAudio = ListBoxHistory.SelectedItem.ToString();
+                DialogResult = true;
+            }
+        }     
+    }    
+}
+
+
